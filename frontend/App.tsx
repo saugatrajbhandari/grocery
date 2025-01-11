35a795d7f4573566f8ea5f3a2eb581dd95dd8505
Saugat Rajbhandari <saugatrajbhandari47@gmail.com>
2025-01-11 23:09:40 +0545

    refactor

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from './src/Navigation/Navigation';
+import 'react-native-gesture-handler';
 
 const App = () => {
   return <Navigation />;
--- frontend/src/Navigation/CustomerLogin.tsx
+++ frontend/src/Navigation/CustomerLogin.tsx
@@ -1,12 +1,21 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const CustomerScreen = () => {
   return (
-    <View>
-      <Text>CustomerScreen</Text>
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <Text>dfassdfa</Text>
+      </View>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default CustomerScreen;
